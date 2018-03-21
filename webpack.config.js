afcae90150cea188de1b8bafeb75ddab2894feed
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-21 08:09:54 -0300

    Configured the architecture of the project and the webpack for to manage our bundles.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            loader: 'babel-loader',
+            test: /\.js$/,
+            exclude: /node_modules/
+        }]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'public')
+    }
+}
