07b29225039d5be6ad670ae9f2cd67ad26ea8369
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-21 08:42:45 -0300

    Testing the components usage in this project.

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ViaCEPApp from './components/ViaCEPApp';
+
+ReactDOM.render(<ViaCEPApp reason="Because still is in development process!" />, document.getElementById('root'));
---
+++ src/components/ViaCEPApp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+export default class ViaCEPAPP extends Component {
+    render () {
+        return (
+            <div>
+                <h1>Comming Soon</h1>
+                <h2>{this.props.reason}</h2>
+            </div>
+        )
+    }
+}
