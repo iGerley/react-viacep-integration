5b83692f0328dc30b7ee033a45a6d510e8085b76
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-21 08:35:59 -0300

    Renamed the local path of the bundle generated. Added the bundle.js and favicon files at index.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, 'public'),
+        path: path.resolve(__dirname, 'public/js'),
         filename: 'bundle.js'
     },
     module: {
