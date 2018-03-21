7319fb84933763995a7bcbf324dc54893dc493ec
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-21 08:15:27 -0300

    Correcting the bundle filename path.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: path.resolve(__dirname, 'src'),
+        path: path.resolve(__dirname, 'public'),
         filename: 'bundle.js'
     },
     module: {
