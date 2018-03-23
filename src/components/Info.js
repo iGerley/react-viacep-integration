bac93deab5cab35bd7425d2575ef7a0c553e353f
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-23 10:06:09 -0300

    Added the CEP informations.

--- src/components/Info.js
+++ src/components/Info.js
@@ -2,7 +2,18 @@ import React from 'react';
 
 const Info = (props) => (
     <div>
-        { Object.keys(props.data).length > 1 && <p>test</p> }
+        {Object.keys(props.data).length > 1 && (
+            <div>
+                <p>CEP: {props.data.cep}</p>
+                <p>Logradouro: {props.data.logradouro}</p>
+                <p>Complemento: {props.data.complemento}</p>
+                <p>Bairro: {props.data.bairro}</p>
+                <p>Localidade: {props.data.localidade}</p>
+                <p>UF: {props.data.uf}</p>
+                <p>Unidade: {props.data.unidade}</p>
+                <p>IBGE: {props.data.ibge}</p>
+            </div>
+        )}
     </div>
 );
 
