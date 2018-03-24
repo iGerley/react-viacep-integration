4d5f46d1812a7ae9e6353e986c40e538ff55691d
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-24 08:10:15 -0300

    Configured the CSS and SCSS loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,21 @@ module.exports = {
         filename: 'bundle.js'
     },
     module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }]
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            }
+        ]
     },
     devtool: 'cheap-module-eval-source-map',
     devServer: {
