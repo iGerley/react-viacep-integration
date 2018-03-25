1a6768ac2359942c3f3825df9b32ed0d8bef2d73
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-25 07:59:39 -0300

    Imported the style files.

--- src/app.js
+++ src/app.js
@@ -2,4 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ViaCEPApp from './components/ViaCEPApp';
 
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
 ReactDOM.render(<ViaCEPApp />, document.getElementById('root'));
