026ff88ecf008958229bc08bf6c7afba68a99a3c
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-25 08:15:55 -0300

    Including the importants base styles.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ViaCEPApp from './components/ViaCEPApp';
-
 import 'normalize.css/normalize.css';
 import './styles/style.scss';
 
