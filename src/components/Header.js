85e64a67b25b7b367b4c56d28ebb4f48b1d61428
iGerley <gerleyadrianomiranda@gmail.com>
2018-03-25 08:58:54 -0300

    Changed the Header default props.

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,14 @@ const Header = (props) => (
     <div className="header">
         <div className="container">
             <h1 className="header__title">{props.title}</h1>
-            {props.subtitle && <h2>{props.subtitle}</h2>}
+            {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
         </div>
     </div>
 );
 
 Header.defaultProps = {
-    title: 'React ViaCEP Integration'
+    title: 'ViaCEP',
+    subtitle: 'Integration beteween the React library and ViaCEP free API'
 };
 
 export default Header;
